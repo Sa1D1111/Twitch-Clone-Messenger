f705437f69eef3a9db16c3f68a0e15e004fcc68a
Sal <87517930+Sa1D1111@users.noreply.github.com>
2022-02-03 16:34:40 -0800

    Add files via upload

---
+++ src/components/Video.js
@@ -0,0 +1,55 @@
+import ReactPlayer from 'react-player'
+
+const Video = () => {
+    return (
+        <div className="video-container">
+            <ReactPlayer
+                url={"https://www.youtube.com/watch?v=ZETAkUZfl18"}
+                muted={false}
+                controls={true}
+
+                playing={true}
+                width={'100%'}
+                height={'60%'}
+            />
+            <center><p className ="welcome">!!WELCOME TO MY メッセンジャー by SAL˥∀S</p></center>
+            <center><p className= "welcome2"> l∀SS∀˥ ʎq ɹƎפNƎSSƎW ⅄W O┴ ƎWOƆ˥ƎM</p></center>
+            <center><p className ="welcome">!!WELCOME TO MY メッセンジャー by SAL˥∀S</p></center>
+            <center><p className= "welcome2"> l∀SS∀˥ ʎq ɹƎפNƎSSƎW ⅄W O┴ ƎWOƆ˥ƎM</p></center>
+                <center><a href = "https://osean.world.com">Check here for death</a></center>
+
+            <div>
+            <img src={require("./images/Owl2.gif")} width="500" height="655" id = "owl"/>
+            <img src={require("./images/Owl2.gif")} width="200" height="400" id = "test"/>
+            <img src={require("./images/trip.gif")} width="500" height="500" id="trip"/>
+            <img src={require("./images/snake.gif")} width="200" height="342" id="snake"/>
+            <img src={require("./images/eyes.gif")} width="200" height="700" id = "eyes"/>
+                <img src={require("./images/GatoArt.gif")} width="600" height="338" id = "gato"/>
+            </div>
+        <div>
+            <img src={require("./images/020-3.gif")} width="400" height="433" id = "alice"/>
+            <img src={require("./images/ezgif-5-c0db0d7e13.gif")} width="260" height="350" id = "matrix"/>
+            <img src={require("./images/hypnotoad.gif")} width="100" height="150" id="toad"/>
+        </div>
+            <div className="cubes">
+                <div className="cube" id="twitter"/>
+            </div>
+            <img src={require("./images/Meta.jpg")} width="100" height="150" id="meta" alt={"FUCKOFF MARK"}/>
+            <a href= "https://nakamotoinstitute.org/" className = "easter">HAHA</a>
+        </div>
+
+
+    )
+}
+
+export default Video;
+
+//<a href="https://www.google.com"><img src="images/pic_trulli.jpg" /> JUST SHOW ALREADY!</a>
+//<img src="images/pic_trulli.jpg" alt="Girl in a jacket" width="500" height="600">
+//   <img src={require("./images/pic_trulli.jpg")} ></img>
+
+
+// lOSING EVERYONE IN MY LIFE HAS BEEN A (detrimental?{lie}) BLESSING IN DISGUISE. I'M FINALLY LEARNING AN EXTREMELY USEFUL SKILL THAT WILL HELP ME IN
+// THE LONG RUN AND HOPEFULLY PROVIDE TO SOCIETY IN SOME WAY, I DONT WANT TO CONSUME MORE THAN I PROVIDE
+// . YEAH I FEEL EMPTY, DEAD INSIDE ALL THE TIME, AND YEARN FOR SOME HUMAN INTERACTION... BUT HEY, ATLEAST I CAN CODE NOW RIGHT? =D
+// tes
---
+++ src/styles/customStyles.js
@@ -0,0 +1,25 @@
+export const customStyles = {
+    '--border-radius-md': '0',
+    '--sm-p': '0',
+
+
+    '--bg-gradient-end': '#007bf8',
+    '--bg-gradient-start': '#070a0d',
+    '--black': '#ffffff',
+    '--blue-alice': '#00193d',
+    '--border': '#004fff',
+    '--button-background': '#ffffff',
+    '--button-text': 'rgba(141,37,255,0.97)',
+    '--grey': '#7a7a7a',
+    '--grey-gainsboro': '#2d2f2f',
+    '--grey-whisper': '#1c1e22',
+    '--modal-shadow': '#000000',
+    '--overlay': '#00000066',
+    '--overlay-dark': '#ffffffcc',
+    '--shadow-icon': 'rgba(255,0,0,0.5)',
+    '--targetedMessageBackground': '#302d22',
+    '--transparent': 'transparent',
+    '--white': '#101418',
+    '--white-smoke': '#13151b',
+    '--white-snow': '#042b4f',
+};
